18a958d36948d8d625055f966819264ace810e0c
lirsacc <c.lirsac@gmail.com>
2015-02-28 18:20:39 +0000

    Fix gh-pages script [ci-skip]

--- gh-pages.sh
+++ gh-pages.sh
@@ -7,10 +7,11 @@ tmp_dir=`mktemp -d /tmp/build-lirsac.me-XXXX`
 mv _site/* $tmp_dir
 shopt -s extglob
 rm -r !(.|..|.git*|node_modules)
-git checkout -B gh-pages
+git checkout gh-pages
 mv $tmp_dir/* .
-git commit -am "[Auto] gh-pages updated on $(date)"
-# git push origin gh-pages --force
+git add .
+git commit -m "[Auto] gh-pages updated on $(date)"
+git push origin gh-pages --force
 rm -r !(.|..|.git*|node_modules)
 git checkout master
 
