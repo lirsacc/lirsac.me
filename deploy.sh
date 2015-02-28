4f96b7ba9a5202236b85fe0e7c53ffefe3d55ea2
lirsacc (TravisCI) <c.lirsac@gmail.com>
2015-02-28 21:29:15 +0000

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,16 @@ if [ "$TRAVIS" == "true" ]; then
   git config user.name "lirsacc (travis ci)"
   git config user.email "c.lirsac@gmail.com"
   repo=https://$GITHUB_TOKEN@github.com/$target
-  git remote rename origin.bak
+  git remote rename origin origin.bak
   git remote add origin $repo
   git remote remove origin.bak
+  git fetch --all
 fi
 
 ./build-gh-pages.sh
 
 if [ "$TRAVIS" == "true" ]; then
-  git push -fq $repo gh-pages:gh-pages > /dev/null
+  git push -fq $repo gh-pages:gh-pages
 else
   read -p "Manually push changes to GitHub Pages branch? [y/N] " response
   if [[ "$response" == 'y' ]] || [[ "$response" == 'Y' ]]; then
