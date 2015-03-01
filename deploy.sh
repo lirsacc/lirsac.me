224e0b04157dbd9d284ae2d60807fefd479a74ba
lirsacc (TravisCI) <c.lirsac@gmail.com>
2015-03-01 00:04:59 +0000

    Improve error handling in bash scripts

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,20 @@
 target="lirsacc/lirsac.me.git"
 
 if [ "$TRAVIS" == "true" ]; then
-  echo "Deploying from Travis..."
   git config user.name "lirsacc (travis ci)"
   git config user.email "c.lirsac@gmail.com"
   repo=https://$GITHUB_TOKEN@github.com/$target
-  git remote rename origin origin.bak > /dev/null 2>&1
-  git remote add origin $repo > /dev/null 2>&1
-  git remote remove origin.bak > /dev/null 2>&1
-  git fetch --all > /dev/null 2>&1
+  echo "Setting up correct remote..."
+  git remote rename origin origin.bak
+  git remote add origin $repo  > /dev/null 2>&1 || exit 1
+  git remote remove origin.bak
+  git fetch --all  > /dev/null 2>&1 || exit 1
 fi
 
-./build-gh-pages.sh > /dev/null 2>&1
+./build-gh-pages.sh
 
 if [ "$TRAVIS" == "true" ]; then
-  git push --force --quiet $repo gh-pages:gh-pages > /dev/null 2>&1
+  git push --force --quiet $repo gh-pages:gh-pages > /dev/null 2>&1 || exit 1
 else
   read -p "Manually push changes to GitHub Pages branch? [y/N] " response
   if [[ "$response" == 'y' ]] || [[ "$response" == 'Y' ]]; then
