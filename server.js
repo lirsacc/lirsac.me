b84c49fc55e520df64247418d4bc7107bd933e17
lirsacc <c.lirsac@gmail.com>
2017-09-10 20:54:07 +0200

    reset

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-    logger = require('morgan');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.static(__dirname + '/_site'));
-app.listen(3000);
--- src/assets/bundle.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var $ = require('./scripts/helpers.js');
-var Lazy = require('./scripts/lazy.js');
-
-$.addEvent(document, 'DOMContentLoaded', function () {
-  $.removeClass(document.body, "preload");
-  Lazy.init();
-});
