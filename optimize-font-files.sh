e7ca6d18930ba842008cece6b9288b5adab7a203
lirsacc <c.lirsac@gmail.com>
2020-07-02 22:30:59 +0200

    Change fonts

--- optimize-font-files.sh
+++ optimize-font-files.sh
@@ -4,28 +4,27 @@
 # Based on https://www.zachleat.com/web/css-tricks-web-fonts/ w/o the second
 # stage JS part.
 
-# For the fonts currently in the project:
-# $ LAYOUT_FEATURES=rlig,ccmp,locl ./optimize-font-files.sh static/fonts/inter
-#
+# For the fonts currently in the repo:
+# $ LAYOUT_FEATURES=sups,kern,frac ./optimize-font-files.sh fonts/basiersquare static/fonts/basiersquare
+# $ LAYOUT_FEATURES=sups,frac ./optimize-font-files.sh fonts/basiersquaremono static/fonts/basiersquaremono
 
 # This can take a while.
 
 set -eu -o pipefail
-set -x
-
-SUFFIX=optimized
+# set -x
 
 UNICODE_RANGE='U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD'
 
 # Adapt based on what's available in each font and what you want.
 # You can use https://wakamaifondue.com/ to inspect the source files.
-LAYOUT_FEATURES='ccmp,mark'
+LAYOUT_FEATURES=${LAYOUT_FEATURES:-'ccmp,mark'}
 
 pip install fonttools brotli
 
-rm -rf ${1}/*.${SUFFIX}.woff*
+mkdir -p "${2}"
+rm -rf "${2}/*.woff*"
 
-for source_file in ${1}/*.woff2; do
+for source_file in "${1}"/*.woff2; do
 
   name=$(basename "${source_file%.*}")
   echo "Optimizing ${name}"
@@ -33,8 +32,8 @@ for source_file in ${1}/*.woff2; do
   pyftsubset \
     "${source_file}" \
     --flavor=woff2 \
-    --output-file="${1}/${name}.${SUFFIX}.woff2" \
-    --layout-features=${LAYOUT_FEATURES} \
+    --output-file="${2}/${name}.woff2" \
+    --layout-features="${LAYOUT_FEATURES}" \
     --no-hinting \
     --desubroutinize \
     --unicodes=$UNICODE_RANGE
@@ -42,10 +41,9 @@ for source_file in ${1}/*.woff2; do
   pyftsubset \
     "${source_file}" \
     --flavor=woff \
-    --output-file="${1}/${name}.${SUFFIX}.woff" \
-    --layout-features=${LAYOUT_FEATURES} \
+    --output-file="${2}/${name}.woff" \
+    --layout-features="${LAYOUT_FEATURES}" \
     --no-hinting \
     --desubroutinize \
     --unicodes=$UNICODE_RANGE
-
 done
